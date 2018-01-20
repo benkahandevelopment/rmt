ba49b1516dd7f125f851f35a2b865cb0a05b9cb7
Ben <ben.kahan@live.co.uk>
2018-01-20 21:56:57 +0000

    Continued development of documentation

--- docs/js/iframelinks.js
+++ docs/js/iframelinks.js
@@ -1,7 +1,23 @@
-$(function(){
+/*$(function(){
     $("a").each(function(){
         var $t = $(this);
         $t.attr("href","/"+parent.document.location.href.split("/").pop().split("#")[0]+$t.attr("href"));
         $t.attr("target","_parent");
+    });
+});*/
+
+$(function(){
+    /*$("a").each(function(){
+        var $t = $(this);
+        $t.attr("href","/options.html"+$t.attr("href"));
+        $t.attr("target","_parent");
+    });*/
+
+    $("a").click(function(){
+        var $t = $(this);
+        console.log("about to reload...");
+        parent.window.location.href="/"+parent.document.location.href.split("/").pop().split("#")[0]+$t.attr("href");
+        parent.location.reload();
+        e.preventDefault();
     });
 });
--- js/iframelinks.js
+++ js/iframelinks.js
@@ -1,7 +1,15 @@
 $(function(){
-    $("a").each(function(){
+    /*$("a").each(function(){
         var $t = $(this);
-        $t.attr("href","options.html"+$t.attr("href"));
+        $t.attr("href","/options.html"+$t.attr("href"));
         $t.attr("target","_parent");
+    });*/
+
+    $("a").click(function(){
+        var $t = $(this);
+        console.log("about to reload...");
+        parent.window.location.href="/options.html"+$t.attr("href");
+        parent.location.reload();
+        e.preventDefault();
     });
 });
